3a915f8a3fb848cc87169371342ae405a817b846
Gopinath Vijayarangan <gvijayarangan@unomaha.edu>
2017-03-06 13:39:53 -0600

    Demo2 final

--- app/Http/Controllers/CenterController.php
+++ app/Http/Controllers/CenterController.php
@@ -41,9 +41,9 @@ class CenterController extends Controller
             'cntr_add1' => 'required|string|Max:50',
             'cntr_city' => 'required|string|Max:50',
             'cntr_state' => 'required|string|Max:20',
-            'cntr_zip' => 'required|integer|digits:5',
-            'cntr_phone' => 'integer|digits:10',
-            'cntr_fax' => 'integer|digits:10',
+            'cntr_zip' => 'required|string|digits:5',
+            'cntr_phone' => 'string|digits:10',
+            'cntr_fax' => 'string|digits:10',
         ]);
         $center = new Center();
         $center->cntr_name = $request->cntr_name;
@@ -86,9 +86,9 @@ class CenterController extends Controller
             'cntr_add1' => 'required|string|Max:50',
             'cntr_city' => 'required|string|Max:50',
             'cntr_state' => 'required|string|Max:20',
-            'cntr_zip' => 'required|integer|digits:5',
-            'cntr_phone' => 'integer|digits:10',
-            'cntr_fax' => 'integer|digits:10',
+            'cntr_zip' => 'required|string|digits:5',
+            'cntr_phone' => 'string|digits:10',
+            'cntr_fax' => 'string|digits:10',
         ]);
 
         $UpdateCntr = Center::find($id);
